924cbc6fe21c7a8a04ffcae54813fb6de4d7c16b
SRainheart <sinrainheart@gmail.com>
2025-04-18 20:08:42 +0300

    third  commit

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import tkinter as tk
 from tkinter import messagebox
 
-def on_button_click():
-    messagebox.showinfo("Уведомление", "Вы нажали на кнопку")
-#fff
+def on_button_click_1():
+    messagebox.showinfo("Уведомление", "Вы нажали на первую кнопку")
+
+def on_button_click_2():
+    messagebox.showinfo("Уведомление", "Вы нажали на такую же кнопку")
+
 # Создаем главное окно
 root = tk.Tk()
 root.title("Мое десктопное приложение")
 root.geometry("300x200")  # Размер окна: ширина x высота
 
-# Создаем кнопку по центру
-button = tk.Button(root, text="Нажми меня", command=on_button_click)
-button.pack(expand=True)
+# Создаем первую кнопку
+button1 = tk.Button(root, text="Нажми меня", command=on_button_click_1)
+button1.pack(expand=True)
+
+# Создаем вторую кнопку
+button2 = tk.Button(root, text="И я такая же", command=on_button_click_2)
+button2.pack(expand=True)
 
 # Запускаем главный цикл приложения
 root.mainloop()
